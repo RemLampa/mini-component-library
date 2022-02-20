155fb0be42f7f2100ea0136097268f44ea43c25a
RemLampa <rem@remlampa.com>
2022-02-21 00:01:27 +0800

    :sparkles: implement Select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,17 +1,60 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import BaseIcon from "../Icon";
+import { getDisplayedValue } from "./Select.helpers";
+import VisuallyHidden from "../VisuallyHidden";
+
+const Dropdown = styled.select`
+  font-size: 1em;
+  font-weight: 400;
+  padding: 12px;
+  padding-right: 52px;
+  cursor: pointer;
+
+  appearance: none;
+
+  border: 0;
+  border-radius: 8px;
+
+  color: ${COLORS.gray700};
+
+  position: relative;
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const Wrapper = styled.div`
+  position: relative;
+  display: inline-block;
+`;
+
+const Icon = styled(BaseIcon)`
+  position: absolute;
+  right: 10px;
+  top: 10px;
+
+  color: ${COLORS.gray700};
+
+  ${Dropdown}:hover + & {
+    color: ${COLORS.black};
+  }
+`;
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      <Dropdown value={value} onChange={onChange}>
+        {children}
+      </Dropdown>
+      <Icon id="chevron-down" strokeWidth={2} />
+    </Wrapper>
   );
 };
 
